2efd017c7750bb6dbe99383c37feaed5ea0e8fee
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2015-03-11 02:52:47 +0200

    Initial commit

--- questboard-app/app/models/quest.rb
+++ questboard-app/app/models/quest.rb
@@ -1,4 +1,9 @@
 class Quest < ActiveRecord::Base
 
 	has_many :tasks, dependent: :destroy
+
+	def create_personal_quest(args)
+		quest = self.create(args)
+		# Implement the join table record creation here
+	end
 end
