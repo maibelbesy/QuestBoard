b3b124fdbe4abc21cd3f62f217019fa033c91535
rana.bendary94@gmail.com <rana.bendary94@gmail.com>
2015-03-20 22:36:00 +0200

    LoginLogout #6

--- questboard-app/config/routes.rb
+++ questboard-app/config/routes.rb
@@ -1,4 +1,11 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get    'login'   => 'sessions#new' 
+  post   'login'   => 'sessions#create' , as: :login_user
+  delete 'logout'  => 'sessions#destroy'
+ get 'signup'  => 'users#new'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
