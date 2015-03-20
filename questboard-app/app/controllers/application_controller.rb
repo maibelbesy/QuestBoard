b01ee2782ae1038ebd8ed3f67fc1d0dd02337229
Maibelbesy <Maibelbesy@gmail.com>
2015-03-20 23:55:26 +0200

    Sprint0

--- questboard-app/app/controllers/application_controller.rb
+++ questboard-app/app/controllers/application_controller.rb
@@ -1,8 +1,12 @@
 class ApplicationController < ActionController::Base
+	before_action :authorize_user
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   include SessionsHelper
   protect_from_forgery with: :exception
-  include SessionsHelper
 
+private
+  def authorize_user
+  	redirect_to login_path and return if !logged_in? && request.path != "/login"
+  end
 end
