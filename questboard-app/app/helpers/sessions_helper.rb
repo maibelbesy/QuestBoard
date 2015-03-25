bde73065bcd5e44a1d888227a95aa9fb02e36f33
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2015-03-25 20:12:53 +0200

    Edit profile fixes

--- questboard-app/app/helpers/sessions_helper.rb
+++ questboard-app/app/helpers/sessions_helper.rb
@@ -2,8 +2,7 @@ module SessionsHelper
 	def log_in(user)
 		session[:user_id] = user.id
 	end
-
-<<<<<<< HEAD
+  
   def current_user
     if (user_id = session[:user_id])
       @current_user ||= User.find_by(id: session[:user_id])
