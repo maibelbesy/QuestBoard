7ebe8b7417e28ddca57437fcdcd8380f12f29033
Maibelbesy <Maibelbesy@gmail.com>
2015-04-03 19:56:27 +0200

    reminder#21 used mandrill gem

--- questboard-app/app/models/quest.rb
+++ questboard-app/app/models/quest.rb
@@ -6,36 +6,56 @@ class Quest < ActiveRecord::Base
 		UsersQuest.create(:assignor_id => user.id, :assignee_id => user.id, :quest_id => quest.id)
 	end
 
-require 'net/smtp'
-def send_email(to,opts={})
-  opts[:server]      ||= 'localhost'
-  opts[:from]        ||= 'email@example.com'
-  opts[:from_alias]  ||= 'Example Emailer'
-  opts[:subject]     ||= "You have to complete your task"
-  opts[:body]        ||= "Important stuff!"
+#require 'net/smtp'
+#def send_email(to,opts={})
+ # opts[:server]      ||= 'localhost'
+  #opts[:from]        ||= 'email@example.com'
+  #opts[:from_alias]  ||= 'Example Emailer'
+  #opts[:subject]     ||= "You have to complete your task"
+  #opts[:body]        ||= "Important stuff!"
 
-  msg = <<END_OF_MESSAGE
-From: #{opts[:from_alias]} <#{opts[:from]}>
-To: <#{to}>
-Subject: #{opts[:subject]}
+  #msg = <<END_OF_MESSAGE
+#From: #{opts[:from_alias]} <#{opts[:from]}>
+#To: <#{to}>
+#Subject: #{opts[:subject]}
 
 #{opts[:body]}
-END_OF_MESSAGE
+#END_OF_MESSAGE
 
-  Net::SMTP.start(opts[:server]) do |smtp|
-    smtp.send_message msg, opts[:from], to
-  end
- end
+ # Net::SMTP.start(opts[:server]) do |smtp|
+  #  smtp.send_message msg, opts[:from], to
+  #end
+ #end
  def reminders 
- 	  quests=Quest.all
-    quests.each do |q|
+    @quests=Quest.all
+    @quests.each do |q|
     #userQuest=UsersQuest.find_by_quest_id(q.id)
-    userQuest=UsersQuest.where(:quest_id => q.id).pluck(:assignee_id)
-    user=User.find(userQuest)
+    @userQuest=UsersQuest.where(:quest_id => q.id).pluck(:assignee_id)
+    @user=User.find(userQuest)
     if (q.remind_to == true && DateTime.now == q.reminder)
-   	send_email user.email, :body => "please finish your task asap"
+    send_email #@user.email, :body => "please finish your task asap"
     q.remind_to=false
    end 
  end
 end
+ require 'mandrill' 
+ def send_email 
+m = Mandrill::API.new
+message = {  
+ :subject=> "Hello from the Mandrill API",  
+ :from_name=> "QuestBoard",  
+ :text=>"Hi message, how are you?",  
+ :to=>[  
+   {  
+     :email=> @user.email,  
+     :name=> @user.first_name  
+   }  
+ ],  
+ :html=>"<html><h1>Hi <strong>message</strong>, how are you?</h1></html>",  
+ :from_email=>"QuestBoard@yourdomain.com"  
+}  
+sending = m.messages.send message  
+puts sending
 end
+end
+ 
