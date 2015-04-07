6bd2fedfdcb4396ddd89c111d663379d497c8a0f
Mai Belbesy <Maibelbesy@Mais-MacBook-Air.local>
2015-04-07 19:32:06 +0200

    UpdateQuestStatus #24

--- questboard-app/config/routes.rb
+++ questboard-app/config/routes.rb
@@ -1,25 +1,18 @@
 Rails.application.routes.draw do
 
-  # The priority is based upon order of creation: first created -> highest priority.
-  # See how all your routes lay out with "rake routes".
-
-  # get 'sessions/new'
-
+# The priority is based upon order of creation: first created -> highest priority.
+# See how all your routes lay out with "rake routes".
+# get 'sessions/new'
   root 'quests#index'
-
   # Session routes
   get '/login' => 'sessions#login', as: :login
   post '/login'   => 'sessions#create' , as: :login_user
   get '/signup' => 'sessions#register', as: :register
   post '/signup' => 'sessions#register_user', as: :register_user
   get '/logout'  => 'sessions#destroy', as: :logout_user
-
   get '/users/:id' => 'profile#index', as: :user
-
-
-  # Routes for Quests
-
-  get '/quests' => 'quests#index', as: :quests
+# Routes for Quests
+get '/quests' => 'quests#index', as: :quests
   get '/quests/new' => 'quests#new', as: :new_quest
   get '/quests/pending_quests' => 'quests#pending_quests', as: :pending_quests
   get '/quests/general_quests' => 'quests#general_quests', as: :general_quests
@@ -30,63 +23,56 @@ Rails.application.routes.draw do
   post '/quests/edit/:id' => 'quests#update', as: :update_quest
   post '/quests/new' => 'quests#create', as: :create_quest
   delete '/quests/:id'=> 'quests#destroy', as: :delete_quest
-  
-
-  # Routes for the account web pages
+  #Routes for updating the status of a Quest
+  get '/quests/:id/:string' => 'quests#status', as: :status_quest
+   # Routes for the account web pages
   get '/users/edit/:id' => 'account#edit', as: :user_edit
   # get '/users/:id' => 'account#show', as: :user
   post '/users/edit/:id' => 'account#update', as: :user_update
-
   resources :users
-  # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+# You can have the root of your site routed with "root"
+# root 'welcome#index'
+# Example of regular route:
+#   get 'products/:id' => 'catalog#view'
+# Example of named route that can be invoked with purchase_url(id: product.id)
+#   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
+# Example resource route (maps HTTP verbs to controller actions automatically):
+#   resources :products
 
-  # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+# Example resource route with options:
+#   resources :products do
+#     member do
+#       get 'short'
+#       post 'toggle'
+#     end
+#
+#     collection do
+#       get 'sold'
+#     end
+#   end
+# Example resource route with sub-resources:
+#   resources :products do
+#     resources :comments, :sales
+#     resource :seller
+#   end
 
-  # Example of named route that can be invoked with purchase_url(id: product.id)
-  #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
-
-  # Example resource route (maps HTTP verbs to controller actions automatically):
-  #   resources :products
-
-  # Example resource route with options:
-  #   resources :products do
-  #     member do
-  #       get 'short'
-  #       post 'toggle'
-  #     end
-  #
-  #     collection do
-  #       get 'sold'
-  #     end
-  #   end
-
-  # Example resource route with sub-resources:
-  #   resources :products do
-  #     resources :comments, :sales
-  #     resource :seller
-  #   end
-
-  # Example resource route with more complex sub-resources:
-  #   resources :products do
-  #     resources :comments
-  #     resources :sales do
-  #       get 'recent', on: :collection
-  #     end
-  #   end
-
-  # Example resource route with concerns:
-  #   concern :toggleable do
-  #     post 'toggle'
-  #   end
-  #   resources :posts, concerns: :toggleable
-  #   resources :photos, concerns: :toggleable
-
-  # Example resource route within a namespace:
-  #   namespace :admin do
-  #     # Directs /admin/products/* to Admin::ProductsController
-  #     # (app/controllers/admin/products_controller.rb)
-  #     resources :products
-  #   end
+# Example resource route with more complex sub-resources:
+#   resources :products do
+#     resources :comments
+#     resources :sales do
+#       get 'recent', on: :collection
+#     end
+#   end
+# Example resource route with concerns:
+#   concern :toggleable do
+#     post 'toggle'
+#   end
+#   resources :posts, concerns: :toggleable
+#   resources :photos, concerns: :toggleable
+# Example resource route within a namespace:
+#   namespace :admin do
+#     # Directs /admin/products/* to Admin::ProductsController
+#     # (app/controllers/admin/products_controller.rb)
+#     resources :products
+#   end
 end
