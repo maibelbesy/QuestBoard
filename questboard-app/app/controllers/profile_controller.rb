393bd3c016ebb1675088ed30790f69ce0bf3da58
Mbehairy <m7md.beheiry@gmail.com>
2015-04-07 19:12:36 +0200

    Code Cleaned

--- questboard-app/app/controllers/profile_controller.rb
+++ questboard-app/app/controllers/profile_controller.rb
@@ -1,7 +1,7 @@
 class ProfileController < ApplicationController
-	def index
-		@member = User.find(params[:id])
-		# redirect_to index_path and return if !logged_in && @member.id != @current_user.id
-	end
+  def index
+    @member = User.find(params[:id])
+  # redirect_to index_path and return if !logged_in && @member.id != @current_user.id
+  end
 
 end
--- questboard-app/app/controllers/users_controller.rb
+++ questboard-app/app/controllers/users_controller.rb
@@ -1,22 +1,21 @@
 class UsersController < ApplicationController
+  def show
+    @member = User.find(params[:id])
+    @reviews = UsersQuest.where(:assignee_id => @current_user.id)
+  end
 
-	def show
-		 @member = User.find(params[:id])
-     @reviews = UsersQuest.where(:assignee_id => @current_user.id)
-	end
+  def list
+  end
 
-	def list
-	end
-
-	def new
-		 @user = User.new
-	end
+  def new
+    @user = User.new
+  end
 
   def edit
     @user = User.find(params[:id])
   end
 
-	def create
+  def create
     @user = User.new(user_params)
     if @user.save
       log_in @user
@@ -30,15 +29,16 @@ class UsersController < ApplicationController
   def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
-       flash[:success] = "Profile updated"
-       redirect_to @user
+      flash[:success] = "Profile updated"
+      redirect_to @user
     else
       render 'edit'
     end
-   end
-   
+  end
+
   private
-    def user_params
-      params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
-    end
+
+  def user_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
 end
