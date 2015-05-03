510946ca3978dab4a0a03d4527f6dd8ebcdcd1e9
Ahmed Abdul Badie <badie.ahmed@icloud.com>
2015-05-03 03:32:11 +0300

    Layout/Bug fixes

--- questboard-app/app/models/user.rb
+++ questboard-app/app/models/user.rb
@@ -12,7 +12,6 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-
   validates :password, length: { minimum: 6 }, :on => :create
   validates_confirmation_of :password, length: { minimum: 6 }
   has_many :tasks, foreign_key: "user_id"
